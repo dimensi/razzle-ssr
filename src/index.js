c4ff263dc1a533cbda8a09c013b8893409b41c2a
Nikita Nafranets <n.nafranets@digitalhorizon.ru>
2019-08-01 15:53:54 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-import express from 'express'
+import http from 'http'
+import Loadable from 'react-loadable'
 
-import { render } from 'src/after'
-import routes from 'src/routes'
+let app = require('./server').default
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
+const server = http.createServer(app)
 
-const server = express()
-server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', async (req, res) => {
+let currentApp = app
+
+Loadable.preloadAll()
+  .then(() => {
+    server.listen(process.env.PORT || 3000, error => {
+      if (error) {
+        console.log(error)
+      }
+
+      console.log('🚀 started')
+    })
+  })
+if (module.hot) {
+  console.log('✅  Server-side HMR Enabled!')
+
+  module.hot.accept('./server', () => {
+    console.log('🔁  HMR Reloading `./server`...')
+
     try {
-      const html = await render({
-        req,
-        res,
-        routes,
-        assets,
-      })
-      res.send(html)
+      app = require('./server').default
+      server.removeListener('request', currentApp)
+      server.on('request', app)
+      currentApp = app
     } catch (error) {
       console.error(error)
-      res.json({ message: error.message, stack: error.stack })
     }
   })
-
-export default server
+}
