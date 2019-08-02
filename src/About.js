0a650b13724a44770d34415104e2e2f871eef414
Nikita Nafranets <n.nafranets@digitalhorizon.ru>
2019-08-02 14:28:44 +0300

    wip

--- src/About.js
+++ src/About.js
@@ -3,22 +3,25 @@ import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 import Axios from 'axios'
 
-function About ({ stuff, todo }) {
-  return stuff ? (
-    <div>
-      <Helmet>
-        <title>About</title>
-      </Helmet>
-      about <Link to='/'>Home</Link>
-      {JSON.stringify(todo)}
-    </div>
-  ) : null
-}
+class About extends React.Component {
+  static async getInitialProps ({ req, res, match, history, location, ...ctx }) {
+    const data = await Axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.data)
+    return { stuff: 'more stuffs', todo: data }
+  }
 
-About.getInitialProps = async function ({ req, res, match, history, location, ...ctx }) {
-  const data = await Axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.data)
-  return { stuff: 'more stuffs', todo: data }
+  render () {
+    const { stuff, todo } = this.props
+    return stuff ? (
+      <div>
+        <Helmet>
+          <title>About</title>
+        </Helmet>
+        about <Link to='/'>Home</Link>
+        {JSON.stringify(todo)}
+      </div>
+    ) : null
+  }
 }
 
 export default About
